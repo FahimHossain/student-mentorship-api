c7a4632e8fbbfb93aeb98a5c2acb691b691572f1
Fahim Hossain <zenothermic@gmail.com>
2021-12-09 17:37:53 +0600

    added message in index route

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 /**
  * @design by milon27
  */
-const express = require('express');
-const cors = require('cors')
-const cookieparser = require('cookie-parser');
-const auth_cookie = require('./routers/middleware/authMiddleware')
+const express = require("express");
+const cors = require("cors");
+const cookieparser = require("cookie-parser");
+const auth_cookie = require("./routers/middleware/authMiddleware");
 
-const onConnected = require('./socket/main');
-require('dotenv').config();
+const onConnected = require("./socket/main");
+require("dotenv").config();
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));//url encode parse
-app.use(express.json());//json parse
-app.use(cookieparser())// parse http cookie
-app.use(cors({ origin: true, credentials: true }))//enable cros
+app.use(express.urlencoded({ extended: false })); //url encode parse
+app.use(express.json()); //json parse
+app.use(cookieparser()); // parse http cookie
+app.use(cors({ origin: true, credentials: true })); //enable cros
 
 /**
  * @enable cookie store in front end
@@ -24,61 +24,58 @@ app.use(cors({ origin: true, credentials: true }))//enable cros
  * @routers
  */
 
-app.get('/', (req, res) => {
-    res.send("working...")
-})
-
+app.get("/", (req, res) => {
+  res.send("working for fahim...");
+});
 
 // sample code start here
 // @description use for support chat system @author milon27
-app.use('/auth', require('./routers/sample/authRouter'))
+app.use("/auth", require("./routers/sample/authRouter"));
 // @description use for all type of CRUD operation @author milon27
-app.use('/data', require('./routers/sample/dataRouter'));
+app.use("/data", require("./routers/sample/dataRouter"));
 // sample code end here
 
 // @description use for database create and database table create
-app.use('/db', require('./routers/db/dbRouter'))
+app.use("/db", require("./routers/db/dbRouter"));
 
 // @description use for support chat system @author milon27
-app.use('/support', auth_cookie, require('./routers/support/supportRouter'))
+app.use("/support", auth_cookie, require("./routers/support/supportRouter"));
 
 // @description use for  student  @author rijon1810
-app.use('/student', require('./routers/student/studentRouter'));
+app.use("/student", require("./routers/student/studentRouter"));
 
 // @description use for  ao  @author rijon1810
-app.use('/ao', require('./routers/ao/aoRouter'));
+app.use("/ao", require("./routers/ao/aoRouter"));
 
 // @description use for  department  @author rijon1810
-app.use('/department', require('./routers/department/departmentRouter'));
+app.use("/department", require("./routers/department/departmentRouter"));
 
 // @description use for  faculty  @author rijon1810
-app.use('/faculty', require('./routers/faculty/facultyRouter'));
+app.use("/faculty", require("./routers/faculty/facultyRouter"));
 
 // @description use for todo  @author milon27
-app.use('/todo', auth_cookie, require('./routers/todo/todoRouter'));
+app.use("/todo", auth_cookie, require("./routers/todo/todoRouter"));
 
 // @description use for todo  @author milon27
-app.use('/notice', auth_cookie, require('./routers/notice/noticeRouter'));
+app.use("/notice", auth_cookie, require("./routers/notice/noticeRouter"));
 
 // @description use for career  @author milon27
-app.use('/career', auth_cookie, require('./routers/career/careerRouter'));
-
+app.use("/career", auth_cookie, require("./routers/career/careerRouter"));
 
 /**
  * @init_server
  */
 const port = process.env.PORT || 2828;
 const server = app.listen(port, () => {
-    console.log(`server running at http://localhost:${port}`)
-})
-
+  console.log(`server running at http://localhost:${port}`);
+});
 
 //create the socket-io
 const io = require("socket.io")(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
-io.on('connection', onConnected)
+io.on("connection", onConnected);
